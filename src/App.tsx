0ba991b88828c925b18ab8326e9b236c5a1c973d
Oscar <o.arias76510989@gmail.com>
2025-10-04 15:26:03 -0500

    integración de shadcn

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button"
+
+function App() {
+    return (
+        <div className="p-10">
+            <Button>Hola Shadcn</Button>
+        </div>
+    )
+}
+
+export default App
+
