63fbf594fdd651a1ba3d9279148b7647c794aad4
Adriano <redmaen19@gmail.com>
2025-10-29 22:54:14 -0500

    fix: :lipstick: Arreglando errores al logear

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,35 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import RecuperarCuenta from "./pages/RecuperarCuenta";
-import Inicio from "@/pages/Inicio"; // Asegúrate de que no tenga la extensión .tsx aquí
-// 🚨 CORRECCIÓN: Importar el componente de PÁGINA Tickets, NO el ícono 🚨
+import Inicio from "@/pages/Inicio";
 import TicketsPage from "@/pages/Tickets";
 import Chat from "@/pages/Chat.tsx";
-// Eliminamos la importación del ícono: import {Tickets} from "lucide-react";
+
+import { ProtectedRoute } from "./components/auth/ProtectedRoute";
+import { PublicRoute } from "./components/auth/ProtectedLogin";
 
 function App() {
     return (
         <BrowserRouter>
             <Routes>
-                {/* La ruta raíz (/) te lleva al Login. */}
-                <Route path="/" element={<Login />} />
+                <Route path="/" element={
+                    <PublicRoute>
+                        <Login />
+                    </PublicRoute>
+                    }
+                    />
                 <Route path="/recuperar-cuenta" element={<RecuperarCuenta />} />
 
-                {/* Si quieres que Inicio sea el dashboard principal, usa /inicio o /dashboard */}
-                <Route path="/inicio" element={<Inicio />} />
-
-                {/* 🚨 CLAVE: Usamos el componente TicketsPage que contiene la tabla y el sidebar. 🚨 */}
-                <Route path="/tickets" element={<TicketsPage />} />
+                <Route path="/inicio" element={
+                    <ProtectedRoute>
+                        <Inicio />
+                    </ProtectedRoute>
+                }/>
+                <Route path="/tickets" element={
+                    <ProtectedRoute>
+                        <TicketsPage />
+                    </ProtectedRoute>
+                    } />
 
                 <Route path="/chat" element={<Chat />} />
             </Routes>
